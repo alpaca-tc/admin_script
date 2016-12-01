84e6555ddf242148216fbebb33cf8ed3878c4ec8
alpaca-tc <alpaca-tc@alpaca.tc>
2016-12-01 15:54:52 +0900

    Set url helpers

--- lib/admin_script.rb
+++ lib/admin_script.rb
@@ -3,4 +3,5 @@ require 'admin_script/version'
 module AdminScript
   autoload :Base, 'admin_script/base'
   autoload :TypeAttributes, 'admin_script/type_attributes'
+  autoload :Controller, 'admin_script/controller'
 end
--- lib/admin_script/controller.rb
+++ lib/admin_script/controller.rb
@@ -4,35 +4,32 @@ module AdminScript
 
     included do
       before_action :set_admin_script_class, only: [:show, :perform]
+      append_view_path(File.expand_path('../../../app/views', __FILE__))
+
+      helper_method :admin_script_path, :admin_scripts_path
     end
 
-    # def _prefixes
-    #   @_prefixes ||= if request
-    #                    ["#{devise_mapping.scoped_path}/#{controller_name}"] + super
-    #                  else
-    #                    super
-    #                  end
-    # end
-
     def index
-      @admin_scripts = AdminScript.subclasses.sort_by(&:to_s).map(&:new)
+      @admin_scripts = AdminScript::Base.subclasses.sort_by(&:to_s).map(&:new)
+      render 'admin_scripts/index'
     end
 
     def show
       @admin_script = @admin_script_class.new(admin_script_params)
+      render 'admin_scripts/show'
     end
 
-    def perform
+    def update
       @admin_script = @admin_script_class.new(admin_script_params)
 
       success = @admin_script.perform!
-      location = @admin_script.location_url || admin_admin_scripts_path
+      location = @admin_script.location_url || { action: :index }
 
       if success
         redirect_to location, notice: t('messages.success')
       else
         flash.now[:alert] = t('messages.fail')
-        render :show, status: :unprocessable_entity
+        render 'admin_scripts/show', status: :unprocessable_entity
       end
     end
 
@@ -45,7 +42,17 @@ module AdminScript
     end
 
     def set_admin_script_class
-      @admin_script_class = AdminScript.find_class(params[:id])
+      @admin_script_class = AdminScript::Base.find_class(params[:id])
+    end
+
+    # TODO: 適当すぎるのでrouterに載せる
+    def admin_script_path(id)
+      param = id&.to_param || id
+      "/admin_scripts/#{param}"
+    end
+
+    def admin_scripts_path
+      '/admin_scripts'
     end
   end
 end
