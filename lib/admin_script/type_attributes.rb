d3d273f6b6b1327a97e759c478e88b20e7d10ada
alpaca-tc <alpaca-tc@alpaca.tc>
2017-01-06 20:42:31 +0900

    Allow active record type caster

--- lib/admin_script/type_attributes.rb
+++ lib/admin_script/type_attributes.rb
@@ -1,5 +1,5 @@
 require 'active_support/concern'
-require 'active_model/type'
+require 'active_model'
 
 module AdminScript
   # Define method to cast value
@@ -10,13 +10,25 @@ module AdminScript
       def type_attribute(name, cast_type)
         define_method name do
           value = instance_variable_get(:"@#{name}")
-          ActiveModel::Type.lookup(cast_type).cast(value)
+          AdminScript::TypeAttributes.cast_value(cast_type, value)
         end
 
         define_method "#{name}=" do |decibels|
-          value = ActiveModel::Type.lookup(cast_type).cast(decibels)
+          value = AdminScript::TypeAttributes.cast_value(cast_type, decibels)
           instance_variable_set(:"@#{name}", value)
         end
+      end
+    end
+
+    if defined?(ActiveModel) && ActiveModel.gem_version >= Gem::Version.new('5.0.0')
+      def self.cast_value(cast_type, value)
+        ActiveModel::Type.lookup(cast_type).cast(value)
+      end
+    else
+      require 'active_record'
+
+      def self.cast_value(cast_type, value)
+        ActiveRecord::Type.const_get(cast_type.to_s.classify).new.type_cast_from_user(value)
       end
     end
   end
