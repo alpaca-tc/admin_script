346a240cda6ea848f79a3fd91c27486a82fe1302
alpaca-tc <alpaca-tc@alpaca.tc>
2017-01-08 00:24:47 +0900

    Create generator to initialize admin_script

---
+++ lib/generators/admin_script/install_generator.rb
@@ -0,0 +1,13 @@
+require 'rails/generators'
+
+module AdminScript
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+    desc 'AdminScript installation generator'
+
+    def install
+      route("mount AdminScript::Engine => '/admin_script'")
+      template 'initializer.rb', 'config/initializers/admin_script.rb'
+    end
+  end
+end
---
+++ lib/generators/admin_script/templates/initializer.rb
@@ -0,0 +1,7 @@
+AdminScript.configure do |config|
+  # Configure the parent class responsible to render views.
+  # config.parent_controller = 'ActionController::Base'
+
+  # Configure the path to admin script classes.
+  # config.admin_script_paths = [Rails.root.join('app', 'models', 'admin_script').to_s]
+end
