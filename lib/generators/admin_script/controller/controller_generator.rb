2ddc46540701447e86c095e0eb84199951623fa4
alpaca-tc <alpaca-tc@alpaca.tc>
2017-01-08 07:12:56 +0900

    Generate custom controller

---
+++ lib/generators/admin_script/controller/controller_generator.rb
@@ -0,0 +1,21 @@
+require 'rails/generators'
+
+module AdminScript
+  module Generators
+    class ControllerGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+      desc 'AdminScript custom controller generator'
+
+      def install
+        template 'controller.rb', "app/controllers/#{file_path}_controller.rb"
+        template 'layout.html.slim', 'app/views/layouts/admin_script/application.html.slim'
+
+        inject_into_file 'config/initializers/admin_script.rb', after: "AdminScript.configure do |config|" do <<-RUBY
+
+  config.controller_path = '/#{file_path}'
+        RUBY
+        end
+      end
+    end
+  end
+end
