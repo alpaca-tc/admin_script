03376a7f5ef99a586f5f52635a809bba68f3f7b3
alpaca-tc <alpaca-tc@alpaca.tc>
2017-01-08 21:30:41 +0900

    Fixes controller generator

--- lib/generators/admin_script/controller/controller_generator.rb
+++ lib/generators/admin_script/controller/controller_generator.rb
@@ -9,12 +9,13 @@ module AdminScript
       def install
         template 'controller.rb', "app/controllers/#{file_path}_controller.rb"
         template 'layout.html.slim', 'app/views/layouts/admin_script/application.html.slim'
+        template '../../install/templates/initializer.rb', 'config/initializers/admin_script.rb'
 
         inject_into_file 'config/initializers/admin_script.rb', after: "AdminScript.configure do |config|" do <<-RUBY
 
   config.controller = '#{class_name}'
         RUBY
-        end
+       end
       end
     end
   end
