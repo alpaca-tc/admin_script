6fb301dfd445dcc4dc6ac811ce3350516013bc17
alpaca-tc <alpaca-tc@alpaca.tc>
2017-12-21 21:09:45 +0900

    Install bulma

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,14 @@
-console.log('hello')
+'use strict';
+
+import 'bulma/css/bulma.css';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   jQuery('[data-behaviour~=datepicker]').datetimepicker({
+//     showClose: true,
+//     format: 'YYYY/MM/DD'
+//   });
+//
+//   jQuery('[data-behaviour~=datetimepicker]').datetimepicker({
+//     showClose: true
+//   });
+// }, false);
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,54 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require("webpack");
 var path = require('path');
 
 module.exports = {
   entry: {
-    application: path.join(__dirname, 'app', 'javascript', 'application.js')
+    application: path.join(__dirname, 'app', 'javascript', 'application.js'),
   },
   output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'app', 'assets', 'javascripts', 'admin_script'),
+    filename: 'javascripts/admin_script/[name].js',
+    path: path.join(__dirname, 'app', 'assets'),
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.css$/,
-        loader: "style!css",
-        exclude: /node_modules/
-      }, {
-        loader: 'sass-loader',
-        test: /\.sass$/,
-        exclude: /node_modules/,
+        test: /\.(jpg|jpeg|png|gif|svg)$/i,
+        loader: 'file-loader',
         options: {
-          sourceMap: true
+          publicPath: '',
+          outputPath: path.join('images', 'admin_script', '/'),
+          name: '[name].[ext]',
         }
       }, {
-        test: /\.(js|jsx)?(\.erb)?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
+        test: /\.(eot|otf|ttf|woff|woff2)$/i,
+        loader: 'file-loader',
+        options: {
+          publicPath: '',
+          outputPath: path.join('fonts', 'admin_script', '/'),
+          name: '[name].[ext]',
+        }
+      }, {
+        test: /\.(scss|sass|css)$/i,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { minimize: true } },
+            'resolve-url-loader'
+          ]
+        })
       }
     ]
   },
+  resolveLoader: {
+    modules: ['node_modules']
+  },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    // new webpack.optimize.UglifyJsPlugin(),
+    new ExtractTextPlugin('stylesheets/admin_script/[name].css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
   ],
 };
