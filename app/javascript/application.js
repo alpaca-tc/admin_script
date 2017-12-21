d6ffb2adc409343f8e8093a96513363fbee38278
alpaca-tc <alpaca-tc@alpaca.tc>
2017-12-21 21:23:37 +0900

    Install pikaday

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,22 @@
 'use strict';
 
 import 'bulma/css/bulma.css';
+import 'pikaday/css/pikaday.css';
+import Pikaday from 'pikaday';
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   jQuery('[data-behaviour~=datepicker]').datetimepicker({
-//     showClose: true,
-//     format: 'YYYY/MM/DD'
-//   });
-//
-//   jQuery('[data-behaviour~=datetimepicker]').datetimepicker({
-//     showClose: true
-//   });
-// }, false);
+function datepicker(element) {
+  new Pikaday({
+    field: element,
+    format: 'YYYY/MM/DD'
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('[data-behaviour~=datepicker]').forEach(function(element) {
+    datepicker(element);
+  });
+
+  document.querySelectorAll('[data-behaviour~=datetimepicker]').forEach(function(element) {
+    datepicker(element);
+  });
+}, false);
