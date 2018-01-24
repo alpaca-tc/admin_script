eb5ca6fa583302eb4b4d7301a27dc2be987bcf55
alpaca-tc <alpaca-tc@alpaca.tc>
2018-01-24 22:28:48 +0900

    Load locales automatically

--- lib/admin_script/engine.rb
+++ lib/admin_script/engine.rb
@@ -2,10 +2,6 @@ module AdminScript
   class Engine < ::Rails::Engine
     isolate_namespace AdminScript
 
-    initializer 'admin_script.i18n' do |app|
-      app.config.i18n.load_path += gem_locale_files(app)
-    end
-
     initializer 'admin_script.load_subclasses', before: :set_clear_dependencies_hook do |app|
       path = app.paths['app/models'].first
 
@@ -35,16 +31,6 @@ module AdminScript
     config.after_initialize do
       AdminScript::Engine.routes.default_url_options =
         AdminScript.configuration.default_url_options.presence || Rails.application.routes.default_url_options
-    end
-
-    private
-
-    def gem_locale_files(app)
-      array = Array.wrap(app.config.i18n.available_locales)
-      available_locales = array.blank? ? '*' : "{#{array.join ','}}"
-
-      locale_pattern = File.expand_path("../../../config/locales/admin_script/#{available_locales}.yml", __FILE__)
-      Dir[locale_pattern]
     end
   end
 end
