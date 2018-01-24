bec5b9467ac420ef2b12a1b07675bb264e19181a
alpaca-tc <alpaca-tc@alpaca.tc>
2018-01-24 22:50:41 +0900

    Install rails-ujs and turbolinks

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,39 @@
 'use strict';
 
 import 'bulma/css/bulma.css';
-import 'pikaday/css/pikaday.css';
-import Pikaday from 'pikaday';
+import 'pikaday-time/css/pikaday.css';
+import Turbolinks from 'turbolinks';
+import Rails from 'rails-ujs';
+import Pikaday from 'pikaday-time';
+
+Turbolinks.start();
+Rails.start();
 
 function datepicker(element) {
   new Pikaday({
+    showTime: false,
+    use24hour: false,
     field: element,
     format: 'YYYY/MM/DD'
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('[data-behaviour~=datepicker]').forEach(function(element) {
+function datetimepicker(element) {
+  new Pikaday({
+    showTime: true,
+    showSeconds: true,
+    use24hour: true,
+    field: element,
+    format: 'YYYY/MM/DD HH:mm:ss'
+  });
+}
+
+document.addEventListener('turbolinks:load', function() {
+  Rails.$('[data-behaviour~=datepicker]').forEach(function(element) {
     datepicker(element);
   });
 
-  document.querySelectorAll('[data-behaviour~=datetimepicker]').forEach(function(element) {
-    datepicker(element);
+  Rails.$('[data-behaviour~=datetimepicker]').forEach(function(element) {
+    datetimepicker(element);
   });
-}, false);
+});
