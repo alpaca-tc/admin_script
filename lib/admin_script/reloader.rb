bff0c2e4456ff6cdc0a484db84cbc0ef1f0530ff
alpaca-tc <alpaca-tc@alpaca.tc>
2019-01-28 00:30:20 +0900

    Remove polyfill of rails 4

--- lib/admin_script/reloader.rb
+++ lib/admin_script/reloader.rb
@@ -1,14 +1,7 @@
 module AdminScript
   module Reloader
     def self.file_watcher
-      case Rails.version[0]
-      when '4'
-        ActiveSupport::FileUpdateChecker
-      when '5'
-        Rails.application.config.file_watcher || ActiveSupport::FileUpdateChecker
-      else
-        raise 'Unsupported rails version!'
-      end
+      Rails.application.config.file_watcher || ActiveSupport::FileUpdateChecker
     end
   end
 end
